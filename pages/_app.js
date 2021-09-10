9cb8795834ea8bda6123695e67a1d68c2dc97145
maxet <cyberglebov@gmail.com>
2021-09-10 20:14:05 +0300

    GuessTime!

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
 import '../styles/slider.css'
+// import '../styles/guess.css'
+
 // import '../styles/normalize.css'
 // import '../styles/webflow.css'
 function MyApp({ Component, pageProps }) {
