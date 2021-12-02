8a5a9797d36e04d8fc068ea52b320b382ec2d4f1
maxet <cyberglebov@gmail.com>
2021-12-02 22:10:33 +0300

    Hey

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -30,8 +30,39 @@ const bananaApi = async(req, res) => {
       "name": tokenName,
       "description": "During COVID-19, people almost stopped traveling. Because of this, their suitcases stopped fulfilling their function. They have been lying around and gathering dust in people's homes for a long time. This affected the suitcases so much that they began to go crazy. The CrazySuitcases ran away from their homes. Our team rescued the crazy suitcases. But they need new homes. Only you can help them and shelter them!",
       "tokenId" : parseInt(query),
-      "image": `https://crazysuitcases.club/images/norev.jpg`,
-      "external_url":"https://crazysuitcases.club/"
+      "image": `https://gateway.pinata.cloud/ipfs/${trait["imageIPFS"]}`,
+      "external_url":"https://crazysuitcases.club/",
+      "attributes": [          
+          {
+            "trait_type": "Background",
+            "value": trait["Background"]
+          },
+          {
+            "trait_type": "Case Color",
+            "value": trait["Case Color"]
+          },
+          {
+            "trait_type": "Face",
+            "value": trait["Face"]
+          },
+          {
+            "trait_type": "Hat",
+            "value": trait["Hat"]
+          },
+          {
+            "trait_type": "Pants Color",
+            "value": trait["Pants Color"]
+          },
+          {
+            "trait_type": "Pants Pattern",
+            "value": trait["Pants Pattern"]
+          },
+          {
+            "trait_type": "Hands",
+            "value": trait["Hands"]
+          },
+  
+      ]
     }
     
     console.log(metadata)
